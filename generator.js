dd91eead1f67f5ce31cecdfe57623f9f9d0b59c8
Zinger Marat <zingermarat@gmail.com>
2025-09-08 17:52:22 +0300

    Refactor mongoose connection and update package-lock.json; add MongoDB playground examples

--- generator.js
+++ generator.js
@@ -1,29 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const fs = require("fs")
 const data = JSON.parse(fs.readFileSync("people.json"))
 
-mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true })
+mongoose.connect(
+  "mongodb+srv://zingermarat_db_user:lHJVG77z07ID1Lj1@cluster0.4beyfld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  { useNewUrlParser: true }
+)
 
-const Person = mongoose.model('Person', new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    currentCompany: {
+const Person = mongoose.model(
+  "Person",
+  new mongoose.Schema(
+    {
+      firstName: String,
+      lastName: String,
+      currentCompany: {
         industry: String,
-        name: String
-    },
-    currentPosition: String,
-    previousCompanies: [
+        name: String,
+      },
+      currentPosition: String,
+      previousCompanies: [
         {
-            industry: String,
-            name: String
-        }
-    ],
-    salary: Number
-}, { collection: "linkedon" }, {multi: true}))
+          industry: String,
+          name: String,
+        },
+      ],
+      salary: Number,
+    },
+    { collection: "linkedon" },
+    { multi: true }
+  )
+)
 
 const onInsert = function (err, docs) {
-    if (err) { console.log(err) }
-    else { console.info('Done'); mongoose.disconnect() }
+  if (err) {
+    console.log(err)
+  } else {
+    console.info("Done")
+    mongoose.disconnect()
+  }
 }
 
 Person.collection.insertMany(data, onInsert)
